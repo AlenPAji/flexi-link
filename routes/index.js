e31e9af09e9514f66f846f0f94e507aced4fa74f
Anish V Mathew <anishvmathew02@gmail.com>
2024-03-15 22:58:13 +0530

    15-03-AVM

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('./home/Homeindex');
 });
 
+
+router.get('/login', function(req, res, next) {
+  res.render('./gym-owner/login');
+});
+router.get('/register', function(req, res, next) {
+  res.render('./gym-owner/register');
+});
+router.get('/registergym', function(req, res, next) {
+  res.render('./gym-owner/registergym');
+});
+router.get('/owner-dashboard', function(req, res, next) {
+  res.render('./gym-owner/owner-dashboard');
+});
+router.get('/gymimage', function(req, res, next) {
+  res.render('./gym-owner/gymimage');
+});
+router.get('/getaddress', function(req, res, next) {
+  res.render('./gym-owner/getaddress');
+});
+router.get('/check', function(req, res, next) {
+  res.render('./gym-owner/check');
+});
+
+
+
 module.exports = router;
